9f1f0cd5e1f2f91adf69938f2051367fccb55511
sunchangtan <sunchangtan@gmail.com>
2020-09-21 22:33:04 +0800

    目录调整

--- src/sort/bubble_sort.c
+++ src/sort/bubble_sort.c
@@ -31,22 +31,3 @@ void print_ints(int data_len, void *datas[]) {
   printf("\n");
 }
 
-int main() {
-
-  int datas[] = {9, 6, 8, 7, 3, 5, 0};
-  int data_len = sizeof(datas) / sizeof(int);
-
-  void **temp_datas = (void **)malloc(sizeof(void *) * data_len);
-
-  for (int i = 0; i < data_len; i++) {
-    temp_datas[i] = &datas[i];
-  }
-
-  print_ints(data_len, temp_datas);
-
-  bubble_sort(data_len, temp_datas, compare_int, print_ints);
-
-  free(temp_datas);
-
-  return 0;
-}
---
+++ src/sort/bubble_sort.h
@@ -0,0 +1,1 @@
+void bubble_sort(int data_len, void* datas[], compare_function compare, print_datas print);
